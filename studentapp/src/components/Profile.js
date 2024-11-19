924e294739577952f37d4e707855090d1b91cf5e
Dinuki85 <139613301+Dinuki85@users.noreply.github.com>
2024-11-19 14:27:52 +0000

    Profile Components added

---
+++ studentapp/src/components/Profile.js
@@ -0,0 +1,15 @@
+export default function(props){
+    return(
+        <>
+        <table>
+            <tr>
+                <td rowSpan={7}>
+                  <img src={require('../assets/img/1.jpg')}  alt="Profile Pictures"/> 
+                </td>
+                <td>ID</td>
+            </tr>
+        </table>
+        </>
+
+    );
+}
